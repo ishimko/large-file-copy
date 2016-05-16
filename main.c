c478f4e6bb7a1360b3f45f6fe9003b402019fb7b
vanashimko <vana.shimko@gmail.com>
2016-05-16 19:40:36 +0300

    opening files for copying

--- main.c
+++ main.c
@@ -1,10 +1,33 @@
 #include <libgen.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <memory.h>
+#include <errno.h>
+#include <unistd.h>
+#include <limits.h>
 #include "error_printer.h"
 
 #define ARGS_COUNT 4
-
+#define BUFFER_SIZE 4096
 char *MODULE_NAME;
+
+int copy_file(const char *src_path, const char *dest_path, int processes_count) {
+    char buffer[BUFFER_SIZE];
+    int src, dest;
+
+    if ((src = open(src_path, O_RDONLY)) == -1) {
+        print_error(MODULE_NAME, strerror(errno), src_path);
+        return -1;
+    }
+
+    if ((dest = open(dest_path, O_WRONLY | O_CREAT | O_EXCL)) == 1) {
+        print_error(MODULE_NAME, strerror(errno), dest_path);
+        if (close(src) == -1) {
+            print_error(MODULE_NAME, strerror(errno), src_path);
+        }
+        return -1;
+    }
+}
 
 int main(int argc, char *argv[]) {
     MODULE_NAME = basename(argv[0]);
@@ -20,6 +43,11 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    char *src_path = argv[1];
-    char *dest_path = argv[2];
+    char dest_path[PATH_MAX];
+    char src_path[PATH_MAX];
+
+    realpath(argv[1], src_path);
+    realpath(argv[2], dest_path);
+
+    return copy_file(src_path, dest_path, processes_number);
 }
